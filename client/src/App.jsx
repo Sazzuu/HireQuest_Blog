65411732541ed5255eed7ee7703ae783bf1b3932
Sazzuu <sahishnagongani123@gmail.com>
2024-06-07 00:11:14 +0530

    commit chanes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signout from './pages/Signout'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
 export default function App() {
   return (
-    <div>main</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/Home" element = {<Home />}/>
+        <Route path = "/about" element = {<About />}/>
+        <Route path = "/Signin" element = {<Signin />}/>
+        <Route path = "/Signout" element = {<Signout />}/>
+        <Route path = "/Dashboard" element = {<Dashboard />}/>
+        <Route path = "/projects" element = {<Projects />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
